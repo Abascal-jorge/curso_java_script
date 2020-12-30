a0efbf9a482ad8ce77759e90f069d8fc5a772c62
jorge abascal lopez <jorgeabascal20@gmail.com>
2020-12-30 00:06:38 -0500

    Async await

--- 40-PROYECTO-GithubJobs/js/app.js
+++ 40-PROYECTO-GithubJobs/js/app.js
@@ -1,5 +1,6 @@
 //Variables de selectores html
 const formulario = document.querySelector("#formulario");
+const resultado= document.querySelector("#resultado");
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -20,23 +21,54 @@ function onSubmitFormulario(e){
     consultarEmpleos(busqueda);
 }
 
-function consultarEmpleos(buscar){
-    const url = `https://jobs.github.com/positions.json?search=${buscar}`;
+async function consultarEmpleos(buscar){
+    const githuburl = `https://jobs.github.com/positions.json?search=${buscar}`;
+    const url = `https://api.allorigins.win/get?url=${ encodeURIComponent(githuburl)}`;
 
-    fetch(url, {mode: 'cors'})
-        .then( respuesta => respuesta.json())
-        .then( empleos => console.log(empleos))
+    const respuesta = await axios(url);
+
+    mostrarHTML( JSON.parse(respuesta.data.contents));
+    //axios.get(url)
+     //   .then( respuesta => mostrarHTML( JSON.parse(respuesta.data.contents)))
+}
+
+function mostrarHTML(data){
+    while( resultado.firstChild ){
+        resultado.firstChild.remove();
+    }
+
+    if( data.length > 0){
+        resultado.classList.add("grid");
+
+        data.forEach(vacante => {
+            const { company, title, type, url } = vacante;
+
+            resultado.innerHTML += `
+                <div class="shadow bg-white p-6 rounded">
+                    <h2 class="text-2xl font-light mb-4">${title}</h2>
+                    <p class="font-bold uppercase">Compañia:  <span class="font-light normal-case">${company} </span></p>
+                    <p class="font-bold uppercase">Tipo de Contrato:   <span class="font-light normal-case">${type} </span></p>
+                    <a class="bg-teal-500 max-w-lg mx-auto mt-3 rounded p-2 block uppercase font-xl font-bold text-white text-center" href="${url}">Ver Vacante</a>
+                </div>
+            `;
+        });
+    }else{
+        mostrarAlerta("No se encontraron vancates");
+    }
 }
 
 function mostrarAlerta(mensaje){
-    const alerta  = document.createElement("p");
-    alerta.classList.add("error");
-    alerta.textContent = mensaje;
 
-    const contenidoForm = document.querySelector(".contenido-form");
-    contenidoForm.appendChild(alerta);
+    if(!document.querySelector(".error")){
+        const alerta  = document.createElement("p");
+        alerta.classList.add("error");
+        alerta.textContent = mensaje;
 
-    setTimeout(() => {
-        alerta.remove();
-    }, 3000);
+        const contenidoForm = document.querySelector(".contenido-form");
+        contenidoForm.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
 }
