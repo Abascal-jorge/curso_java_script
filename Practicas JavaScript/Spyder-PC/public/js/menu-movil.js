fb83471246148b4bc7916ca759db41af4dcc4474
Abascal <jorgeabascal20@gmail.com>
2021-01-15 16:15:33 -0500

    Avance pagina spyder pc

--- Practicas JavaScript/Spyder-PC/public/js/menu-movil.js
+++ Practicas JavaScript/Spyder-PC/public/js/menu-movil.js
@@ -1,13 +1,35 @@
 /*Variables selectorrees*/
 const menuMovil = document.querySelector("#menu-movil");
+
+/*Variables enlaces menu */
 const scrollServices = document.querySelector("#enlace-servicios");
+const trabajadoresEnlace = document.querySelector("#enlace-trabajadores");
+
+/*Variables selectores seccion indicadas */
 const cardServices = document.querySelector("#services-scroll");
+const trabajadores = document.querySelector("#trabajadores-section");
+
+/*Selectores de trabajadores*/
+const trabajadorUno = document.querySelector("#uno");
+const trabajadorDos = document.querySelector("#dos");
+const trabajadorTres = document.querySelector("#tres");
+
+/* Selectores de opciones footer */
+const productos = document.querySelector("#productos");
 
 MyApp();
 
 function MyApp(){
     menuMovil.addEventListener("click", mostrarMenu);
     scrollServices.addEventListener("click", scrollMostrar);
+    trabajadoresEnlace.addEventListener("click", trabajadoresMostrar);
+    /*productos.addEventListener("click", () => {
+        if(productos.parentElement.childNodes[3].style.display){
+            productos.parentElement.childNodes[3].style.display = "block";
+        }else{
+            productos.parentElement.childNodes[3].style.display = "none";
+        }
+    });*/
     //scrollMostrar();
 }
 
@@ -22,5 +44,29 @@ function mostrarMenu(){
 
 function scrollMostrar(e){
     e.preventDefault();
-    window.scroll(0,cardServices.getBoundingClientRect().y);
+    window.scroll(0, cardServices.getBoundingClientRect().y);
 }
+
+function trabajadoresMostrar(e){
+    e.preventDefault();
+    window.scroll(0, trabajadores.getBoundingClientRect().y);
+}
+
+let i = 1;
+setInterval(() => {
+    if(i === 1){
+        trabajadorUno.style.opacity = "1";
+        trabajadorDos.style.opacity = "0";
+        trabajadorTres.style.opacity = "0";
+    }else if(i === 2){
+        trabajadorDos.style.opacity = "1";
+        trabajadorUno.style.opacity = "0";
+        trabajadorTres.style.opacity = "0";
+    }else{
+        trabajadorTres.style.opacity = "1";
+        trabajadorUno.style.opacity = "0";
+        trabajadorDos.style.opacity = "0";
+        i = 0;
+    }
+    i++;
+}, 10000);
